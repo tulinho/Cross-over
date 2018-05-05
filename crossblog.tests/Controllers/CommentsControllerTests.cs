5cd4653c27b5a8be9f64328cc175f24d034b8af9
marco.ferreira <marco.ferreira@dtidigital.com.br>
2018-05-05 19:06:16 -0300

    Cross-over assessment

---
+++ crossblog.tests/Controllers/CommentsControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using crossblog.Controllers;
+using crossblog.Domain;
+using crossblog.Model;
+using crossblog.Repositories;
+using FizzWare.NBuilder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace crossblog.tests.Controllers
+{
+    public class CommentsControllerTests
+    {
+        private CommentsController _commentController;
+        private ArticlesController _articlesController;
+
+        private Mock<IArticleRepository> _articleRepositoryMock = new Mock<IArticleRepository>();
+        private Mock<ICommentRepository> _commentRepositoryMock = new Mock<ICommentRepository>();
+
+        public CommentsControllerTests()
+        {
+            _commentController = new CommentsController(_articleRepositoryMock.Object, _commentRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task Get_NotFound()
+        {
+            // Arrange
+            _commentRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Comment>(null));
+
+            // Act
+            var result = await _commentController.Get(1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as NotFoundResult;
+            Assert.NotNull(objectResult);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsItem()
+        {
+            // Arrange
+            _commentRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Comment>(Builder<Comment>.CreateNew().Build()));
+            var commentDbSetMock = Builder<Comment>.CreateListOfSize(3).Build().ToAsyncDbSetMock();
+            _commentRepositoryMock.Setup(m => m.Query()).Returns(commentDbSetMock.Object);
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(Builder<Article>.CreateNew().Build()));
+            // Act
+            var result = await _commentController.Get(1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+        }
+
+        [Fact]
+        public async Task Post()
+        {
+            // Arrange
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(Builder<Article>.CreateNew().Build()));
+            _commentRepositoryMock.Setup(m => m.InsertAsync(new Comment())).Returns(Task.FromResult<Comment>(Builder<Comment>.CreateNew().Build()));
+
+            // Act
+            var result = await _commentController.Post(1, new CommentModel());
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ crossblog.tests/Controllers/CustomExceptionsTests.cs
@@ -0,0 +1,36 @@
+﻿using crossblog.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace crossblog.tests.Controllers
+{
+    public class CustomExceptionsTests
+    {
+
+        [Fact]
+        public async Task HttpStatusCodeException_Constructor1()
+        {
+            var exception = new HttpStatusCodeException(404);
+            Assert.Equal(exception.StatusCode, 404);
+        }
+
+        [Fact]
+        public async Task HttpStatusCodeException_Constructor2()
+        {
+            var exception = new HttpStatusCodeException(404, "Not Found");
+            Assert.Equal(exception.StatusCode, 404);
+            Assert.Equal(exception.Message, "Not Found");
+        }
+
+        [Fact]
+        public async Task HttpStatusCodeException_Constructor()
+        {
+            var exception = new HttpStatusCodeException(404, new Exception());
+            Assert.NotNull(exception);
+        }
+
+    }
+}
